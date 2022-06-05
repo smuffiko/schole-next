2f8e90c484a6a6e30fe9e61856e4715ffa4b1234
smuffiko <lucpol96@gmail.com>
2022-06-05 13:37:14 +0200

    added maximum scale (input zoom at iphone safari)

--- components/_App/HeadContent.js
+++ components/_App/HeadContent.js
@@ -1,7 +1,7 @@
 export default function HeadContent() {
   return(
     <>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
     </>
   )
 }
