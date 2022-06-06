f5b6537cae156a235252454391a8b08818172395
smuffiko <luckilkaaa@seznam.cz>
2022-06-06 23:12:36 +0200

    404 added back to prev page

--- pages/404.js
+++ pages/404.js
@@ -1,9 +1,11 @@
 import { Container, Icon } from "semantic-ui-react"
+import Router from "next/router"
 
 const NotFound = ({ t }) => {
   return (<>
     <Container>
       <h1 style={{color:"white"}}><Icon name="x" size="large"/> {t.err404}</h1>
+      <h3 onClick={()=>Router.back()} style={{color:"white",cursor:"pointer"}}><Icon name="arrow alternate circle left outline"/>{t.back}</h3>
     </Container>
   </>)
 }
