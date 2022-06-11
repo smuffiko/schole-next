4844da5f127f84a4af2f787dc1003896e209ec6f
smuffiko <luckilkaaa@seznam.cz>
2022-06-11 21:31:40 +0200

    videoplayer -> reactplayer

--- components/Articles/VideoPlayer.js
+++ components/Articles/VideoPlayer.js
@@ -1,28 +1,17 @@
-import { Player,
-  ControlBar,
-  ReplayControl,
-  ForwardControl,
-  CurrentTimeDisplay,
-  TimeDivider,
-  PlaybackRateMenuButton,
-  VolumeMenuButton } from "video-react"
-import "video-react/dist/video-react.css"
+import ReactPlayer from "react-player/lazy"
 
 const VideoPlayer = ({url}) => {
   return (
     <>
-      <Player>
-        <source src={url} />
-
-        <ControlBar>
-          <ReplayControl seconds={10} order={1.1} />
-          <ForwardControl seconds={30} order={1.2} />
-          <CurrentTimeDisplay order={4.1} />
-          <TimeDivider order={4.2} />
-          <PlaybackRateMenuButton rates={[5, 2, 1, 0.5, 0.1]} order={7.1} />
-          <VolumeMenuButton disabled />
-        </ControlBar>
-      </Player>
+      <div className="player-wrapper">
+        <ReactPlayer
+          url={url}
+          className="react-player"
+          width="100%"
+          height="100%"
+          controls
+        />
+      </div>
     </>
   )
 }
