52ab513b94d5db24511c0aca04d87c45adf54682
smuffiko <luckilkaaa@seznam.cz>
2022-06-15 15:18:16 +0200

    change lang from footer

--- components/_App/Footer.js
+++ components/_App/Footer.js
@@ -1,11 +1,25 @@
-import { Container, Icon, List } from "semantic-ui-react"
+import { useRouter } from "next/router"
+import { Container, Icon, List, Flag } from "semantic-ui-react"
 import Link from "next/link"
+import { changeLang } from "../../utils/local"
+import locales from "../../data/locales.json"
 
 const Footer = ({ t }) => {
+  const router = useRouter()
+
+  const changeLangClick = (newLang) => {
+    changeLang(newLang)
+    t = locales[newLang]
+    router.push(router.pathname)
+  }
   return (
-  <>
+    <>
       <Container textAlign="center">
         <footer>
+          <div style={{color:"white"}}>
+            <Flag style={{cursor:"pointer"}} name="cz" onClick={()=>changeLangClick("cz")}/>
+            <Flag style={{cursor:"pointer"}}  name="gb" onClick={()=>changeLangClick("en")}/>
+          </div>
           <List horizontal divided link size="small" inverted>
             <List.Item>
               <Link href='/contact'>
@@ -29,8 +43,8 @@ const Footer = ({ t }) => {
           </List>
         </footer>
       </Container>
-  </>
-  );
+    </>
+  )
 }
  
-export default Footer;
+export default Footer
