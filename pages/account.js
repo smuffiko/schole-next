bbcd25739ac5d30b84d91a47fdb78eaca7a49d73
smuffiko <luckilkaaa@seznam.cz>
2022-06-16 00:31:56 +0200

    delete account lang

--- pages/account.js
+++ pages/account.js
@@ -1,11 +1,9 @@
 import { Container } from "semantic-ui-react"
-import AccountLang from "../components/Account/AccountLang"
 
 const Account = ({ changeLang }) => {
   return (
   <>
     <Container>
-      <AccountLang changeLang={changeLang}/>
     </Container>
   </>
   )
